dd7fb162a9b77229299a62570ce4e7576d42a4be
drr <drr@civilian-implications.hamilton.local>
2010-10-22 16:58:48 +0100

    more thrashing on the way to a stable build

--- environment.sh
+++ environment.sh
@@ -1,3 +1,7 @@
-export QARCHITECTURE_PATH=`infrastructure-1.27/architecture-path`
+export QARCHITECTURE_PATH=`$HOME/git/stalingrad/infrastructure-1.27/architecture-path`
 export QINSTALLDIR=`pwd`
-export PATH=$HOME/bin/${QARCHITECTURE_PATH}:$PATH
+export PATH=$HOME/git/stalingrad/QobiScheme-1.44/${QARCHITECTURE_PATH}:$PATH
+echo QARCHITECTURE_PATH=`$HOME/infrastructure/architecture-path`
+echo QINSTALLDIR=`pwd`
+echo PATH=$HOME/git/stalingrad/QobiScheme-1.44/${QARCHITECTURE_PATH}:$PATH
+echo done.
