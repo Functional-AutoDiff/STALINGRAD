6c8d6ae57ecd17d7bfccf438d16066b6f38dd9e0
Alexey Radul <axch@mit.edu>
2010-11-24 13:15:29 +0000

    Defined a `make test' target.  This invokes a driver program that reads some VLAD expressions from a file, executes Stalingrad on them, and checks that the results come out as expected.  The idea is to get to a level of coverage where running `make test' should cause one to be reasonably confident that one's change did not break anything.
    Limitations at present:
    - Only the interpreter is tested
    - The test driver is written in MIT Scheme, so you must have MIT Scheme
      on your path to run it.
      sudo apt-get install mit-scheme
    - The test driver and its data files are in the BCL-AD tree, so you must
      have that on hand to run it.
      git clone git://cvs.bcl.hamilton.ie/home/git/BCL-AD.git

---
+++ run-tests.scm
@@ -0,0 +1,29 @@
+
+(set! load/suppress-loading-message? #t)
+
+(define (self-relatively thunk)
+  (if (current-eval-unit #f)
+      (with-working-directory-pathname
+       (directory-namestring (current-load-pathname))
+       thunk)
+      (thunk)))
+
+(define (load-relative filename)
+  (self-relatively (lambda () (load filename))))
+
+(define test-driver "../BCL-AD/vlad/test-vlad.scm")
+
+(if (not (file-readable? test-driver))
+    (begin
+      (display "Fatal: Cannot find test driver in ")
+      (display test-driver)
+      (newline)
+      (display "Fatal: Did you clone BCL-AD in a sister directory?")
+      (newline)
+      (%exit 1)))
+
+(load-relative test-driver)
+(let ((num-failures (show-time run-registered-tests)))
+  (newline)
+  (flush-output)
+  (%exit num-failures))
