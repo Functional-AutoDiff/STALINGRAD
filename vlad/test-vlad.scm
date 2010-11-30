6f8a8f5ade9f45dd99ecc2044eac644223fbed08
Alexey Radul <axch@mit.edu>
2010-11-30 16:29:56 +0000

    Using the test-driver tool in the generated Makefile.

--- vlad/test-vlad.scm
+++ vlad/test-vlad.scm
@@ -66,7 +66,7 @@ FAILURE_REPORTS=$(EXPECTATIONS:.expect=.fail)
 all: $(FAILURE_REPORTS)
 
 %.fail: %.expect
-	-time -o $*.time mit-scheme --heap 6000 --batch-mode --no-init-file --load ../test-driver.scm --eval '(read-and-try-expectation!)' < $^ > $@
+	-time -o $*.time ../tools/test-driver '(read-and-try-expectation!)' < $^ > $@
 
 time-report:
 	find . -name \"*.time\" -printf '%30f   ' -exec head -1 '{}' \\; | sort -g -t ':' -k 2 | sort -g -k 4 -s
